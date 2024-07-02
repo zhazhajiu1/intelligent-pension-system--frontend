b4fe133412acf4dd2191b25f9aa8a0b537a1c7f0
吃遍天下 <759540308@qq.com>
2024-07-02 14:31:57 +0800

    0

--- src/views/sys/staff.vue
+++ src/views/sys/staff.vue
@@ -23,9 +23,9 @@
     <el-card>
       <el-table :data="tableData" border style="width: 95%">
         <el-table-column fixed prop="id" label="序号" width="150"></el-table-column>
-        <el-table-column prop="name" label="姓名" width="200"></el-table-column>
-        <el-table-column prop="sex" label="性别" width="200" :formatter="formatSex"></el-table-column>
-        <el-table-column prop="age" label="年龄" width="200"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="sex" label="性别" width="150" :formatter="formatSex"></el-table-column>
+        <el-table-column prop="age" label="年龄" width="180"></el-table-column>
         <el-table-column prop="phone" label="手机号码" width="350"></el-table-column>
         <el-table-column fixed="right" label="操作" width="200">
           <template slot-scope="scope">
